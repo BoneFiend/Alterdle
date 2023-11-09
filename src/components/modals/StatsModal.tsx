6901d277eb77d98737b48b03226b7bd43b43d079
Mitchell De Nardis <mitchelldenardis@gmail.com>
2023-11-10 00:07:04 +1100

    Accent buttons are consistent and easily changed

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -116,7 +116,7 @@ export const StatsModal = ({
           <div>
             <button
               type="button"
-              className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
+              className="accent-button"
               onClick={() => {
                 shareStatus(
                   solution,
--- src/components/stats/EmigratePanel.tsx
+++ src/components/stats/EmigratePanel.tsx
@@ -45,8 +45,7 @@ export const EmigratePanel = () => {
         disabled={!isCopyButtonEnabled}
         onClick={copyEmigrationCodeToClipboard}
         type="button"
-        className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-left text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:border-gray-200
-          disabled:bg-white disabled:text-gray-900 disabled:focus:outline-none disabled:dark:border-gray-600 disabled:dark:bg-gray-800 disabled:dark:text-gray-400 sm:text-sm"
+        className="accent-button migrate-button"
       >
         {isCopyButtonEnabled && (
           <DuplicateIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
