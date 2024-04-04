b66116e815e0fa44664a90764657577160b30373
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-04-05 01:21:59 +1100

    Updated README and descriptions

--- README.md
+++ README.md
@@ -4,8 +4,22 @@ Alterdle is a fully customisable word guessing game.
 
 For the first time ever it allows users to choose how many words to guess at once (between 1 and 16) as well as how long each word is (between 1 and 11 letters), resulting in 162 unique daily puzzles!!
 
-This game chooses words at random, so it will occasionally choose obscure or inappropriate ones. If you find a particularly offensive wordplease consider contributing to the project
-
-This game was built on top of the open source project [cwackerfuss/react-wordle](https://github.com/cwackerfuss/react-wordle). Made using React, Typescript, and Tailwind.
+This game chooses words at random, so it will occasionally choose obscure or inappropriate ones. If you find a particularly offensive wordplease consider contributing to the project.
 
 [**Try out Alterdle!**](https://alterdle.bonefiend.com/)
+
+## Features
+
+### Full customisation:
+
+<img src="./assets/settings.png" width="300" />
+
+### Correct word animations:
+
+<img src="./assets/correct-word.png" width="400" />
+
+### Detailed stats for each game setting:
+
+<img src="./assets/stats.png" width="250" />
+
+###### Plus a hidden poem!
--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -18,10 +18,10 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         Alterdle is a fully customisable word guessing game.
         <br />
         <br />
-        For the first time ever it allows users to choose how many words to
-        guess at once (between {MIN_NUMBER_OF_WORDS} and {MAX_NUMBER_OF_WORDS}){' '}
-        <i>as well</i> as how long each word is (between {MIN_NUMBER_OF_LETTERS}{' '}
-        and {MAX_NUMBER_OF_LETTERS} letters), resulting in{' '}
+        Choose how long each word is (between {MIN_NUMBER_OF_LETTERS} and{' '}
+        {MAX_NUMBER_OF_LETTERS} letters) <i>as well</i> as how many of them to
+        guess (between {MIN_NUMBER_OF_WORDS} and {MAX_NUMBER_OF_WORDS}) ,
+        resulting in{' '}
         {(MAX_NUMBER_OF_LETTERS - MIN_NUMBER_OF_LETTERS + 1) *
           (MAX_NUMBER_OF_WORDS - MIN_NUMBER_OF_WORDS + 1) -
           MAX_NUMBER_OF_WORDS + // Accounts for only 2 single letter words
@@ -42,15 +42,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         .{' '}
       </p>
       <p className="mt-6 text-sm italic text-stone-700 dark:text-gray-300">
-        This game was built on top of the open source project{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="font-bold underline"
-        >
-          react-wordle
-        </a>
-        , made using React, Typescript, and Tailwind. Check out the code for
-        Alterdle{' '}
+        Check out the code for Alterdle{' '}
         <a
           href="https://github.com/BoneFiend/alterdle"
           className="font-bold underline"
