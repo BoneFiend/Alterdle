c219b8efd0c81762b066293262549de09ff40308
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-07-20 03:19:35 +1000

    move project from create-react-app to vite

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react-swc'
+import env from 'vite-plugin-env-compatible'
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), env()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    },
+  },
+  define: {
+    'process.env': {
+      VITE_GAME_DESCRIPTION: JSON.stringify(process.env.VITE_GAME_DESCRIPTION),
+      VITE_GAME_NAME: JSON.stringify(process.env.VITE_GAME_NAME),
+      VITE_GOOGLE_MEASUREMENT_ID: JSON.stringify(
+        process.env.VITE_GOOGLE_MEASUREMENT_ID
+      ),
+      VITE_PLAUSIBLE_DOMAIN: JSON.stringify(process.env.VITE_PLAUSIBLE_DOMAIN),
+    },
+  },
+})
