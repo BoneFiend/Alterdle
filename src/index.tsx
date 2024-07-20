df0f2455e52980baf54b43754c8fff3507e219f6
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-07-20 13:11:11 +1000

    upgrade from react 17 to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import { AlertProvider } from './context/AlertContext'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const container = document.getElementById('root')!
+
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <AlertProvider>
       <App />
     </AlertProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
