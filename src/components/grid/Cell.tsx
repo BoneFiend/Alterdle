61d5fd5074a127e2e9343c076915765a5868d14d
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-07-28 16:41:13 +1000

    use hardware accelerated rendering of cells

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -34,15 +34,16 @@ export const Cell = ({
   const crampedGrids = numberOfLetters > 7 || numberOfWords > 2
 
   const classes = classnames(
-    'border-solid border-2 mx-0.5 font-bold rounded sm:rounded-md transition-[height] duration-300 ease-in-out select-none',
+    'border-solid border-2 mx-0.5 transform-gpu font-bold rounded sm:rounded-md transition-[height] duration-300 ease-in-out select-none',
     {
       'text-4xl w-14 h-14': !crampedGrids && !helpModal,
       'md:text-4xl text-2xl w-8 h-8 md:w-14 md:h-14 ':
         crampedGrids && !helpModal && isFocussed,
       'md:text-4xl text-2xl w-8 h-6 md:w-14 md:h-10':
         crampedGrids && !helpModal && !isFocussed,
-      'text-3xl sm:text-4xl h-10 w-10 sm:h-14 sm:h-14 ': helpModal,
-      'border-cell-deep shadow-inner': !status || (!value && status === 'null'), // empty cell
+      'text-3xl sm:text-4xl h-10 w-10 sm:h-14 sm:h-14': helpModal,
+      'border-cell-deep shadow-inner ':
+        !status || (!value && status === 'null'), // empty cell
       'border-cell-border-value text-blank': value && status === 'null',
       'bg-absent border-absent text-white shadowed': status === 'absent',
       'bg-correct text-white border-correct shadowed': status === 'correct',
@@ -59,7 +60,7 @@ export const Cell = ({
     <div className={classes} style={{ animationDelay, animationDuration }}>
       <div
         className={classnames(
-          'flex h-full items-center justify-center pb-0.5 transition-shadow',
+          'flex h-full transform-gpu items-center justify-center pb-0.5 transition-shadow',
           shouldReveal && 'animate-letter-flip'
         )}
         style={{ animationDelay, animationDuration }}
