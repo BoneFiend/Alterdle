312b88a78e6d2ea49a976689a92cb4d003c5aa7e
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-11-03 18:50:43 +1100

    add focus visible state to keys

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -33,19 +33,19 @@ export const Key = ({
       aria-label={`${value}${status ? ' ' + status : ''}`}
       className={cn(
         'h-14 sm:h-16 short:h-12 xshort:h-10',
-        'mx-0.5 flex cursor-pointer select-none items-center justify-center rounded-lg font-bold transition-all sm:rounded-xl',
+        'mx-0.5 flex cursor-pointer select-none items-center justify-center rounded-lg font-bold transition-[colors,width,height] sm:rounded-xl',
         {
           'w-[40px] text-lg sm:w-[64px] sm:text-3xl short:w-[40px] short:text-lg':
             !longWidth,
           'w-[65.4px] text-base sm:w-[96px] sm:text-2xl short:w-[65.4px] short:text-base':
             longWidth,
-          'transition ease-in-out': isRevealing,
-          'bg-key text-white hover:bg-key-deep active:bg-key-deeper dark:text-white':
+          'ease-in-out': isRevealing,
+          'bg-key text-white hover:bg-key-deep focus-visible:bg-key-deeper active:bg-key-deeper dark:text-white':
             !status,
           'bg-absent text-white': status === 'absent',
-          'shadowed bg-correct text-white hover:bg-correct-deep active:bg-correct-deeper':
+          'shadowed bg-correct text-white hover:bg-correct-deep focus-visible:bg-correct-deeper active:bg-correct-deeper':
             status === 'correct',
-          'shadowed bg-present text-white hover:bg-present-deep active:bg-present-deeper':
+          'shadowed bg-present text-white hover:bg-present-deep focus-visible:bg-present-deeper active:bg-present-deeper':
             status === 'present',
           'bg-key-deeper transition-none': isActive && !status,
           'bg-correct-deeper transition-none': isActive && status === 'correct',
