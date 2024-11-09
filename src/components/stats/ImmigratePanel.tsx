d72f2506fd6fb3ccda8277cc56364e2ae1d5cef5
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-11-10 00:00:54 +1100

    refactor immigrate panel with react-hook-form and update colours

--- src/components/stats/ImmigratePanel.tsx
+++ src/components/stats/ImmigratePanel.tsx
@@ -1,70 +1,51 @@
-import { useState } from 'react'
+import { useMemo } from 'react'
 
 import { SaveIcon } from '@heroicons/react/outline'
+import { useForm } from 'react-hook-form'
 
 import { Button } from '@ui/inputs/Button'
 
 import { MigrationStats } from '@constants/types'
 
+import cn from '@lib/cn'
 import { decrypt } from '@lib/encryption'
 import {
   saveGameStateToLocalStorage,
   saveStatsToLocalStorage,
 } from '@lib/localStorage'
 
+type DeviceForm = {
+  value: string
+}
+
 export const ImmigratePanel = () => {
-  const [isSaveButtonEnabled, setIsSaveButtonEnabled] = useState(false)
+  const { register, handleSubmit, watch } = useForm<DeviceForm>()
 
-  const textareaClassNames = {
-    valid: ['bg-pink-50', 'dark:bg-stone-700'],
-    invalid: ['bg-red-300', 'dark:bg-red-500'],
-  }
-  const allClassNames = [
-    ...textareaClassNames.valid,
-    ...textareaClassNames.invalid,
-  ]
+  const value = watch('value')
 
-  const handleImmigrationCodeChange = (event: any) => {
-    if (event.target instanceof Element) {
-      const textarea = event.target
-
-      allClassNames.forEach((cn) => textarea.classList.remove(cn))
-
-      setIsSaveButtonEnabled(false)
-
-      const text = textarea.value
-
-      try {
-        const migrationStats = JSON.parse(decrypt(text) ?? '') as MigrationStats
-        if (
-          !migrationStats ||
-          (!migrationStats.gameState && !migrationStats.statistics)
-        ) {
-          textareaClassNames.invalid.forEach((cn) => textarea.classList.add(cn))
-          return
-        }
-
-        textareaClassNames.valid.forEach((cn) => textarea.classList.add(cn))
-        setIsSaveButtonEnabled(true)
-      } catch (error) {
-        textareaClassNames.invalid.forEach((cn) => textarea.classList.add(cn))
+  const isValid = useMemo(() => {
+    try {
+      const migrationStats = JSON.parse(decrypt(value) ?? '') as MigrationStats
+      if (
+        !migrationStats ||
+        (!migrationStats.gameState && !migrationStats.statistics)
+      ) {
+        return false
       }
-    }
-  }
 
-  const handleSaveButton = () => {
-    const textarea = document.getElementById(
-      'immigration-code',
-    ) as HTMLInputElement
+      return true
+    } catch (error) {
+      return false
+    }
+  }, [value])
+
+  const handleSaveButton = ({ value }: DeviceForm) => {
     if (
-      textarea &&
       window.confirm(
         'Are you sure you want to override the statistics on this device? This action is not reversable.',
       )
     ) {
-      var migrationStats = JSON.parse(
-        decrypt(textarea.value) ?? '',
-      ) as MigrationStats
+      var migrationStats = JSON.parse(decrypt(value) ?? '') as MigrationStats
       if (!migrationStats) return
 
       if (migrationStats.gameState) {
@@ -75,14 +56,12 @@ export const ImmigratePanel = () => {
         saveStatsToLocalStorage(migrationStats.statistics)
       }
 
-      alert('The site will now reload.')
-
       window.location.reload()
     }
   }
 
   return (
-    <div className="text-sm">
+    <form className="text-sm" onSubmit={handleSubmit(handleSaveButton)}>
       <label
         htmlFor="message"
         className="mb-2 block text-left text-sm font-medium text-ui-main"
@@ -90,19 +69,18 @@ export const ImmigratePanel = () => {
         Paste your migration code:
       </label>
       <textarea
-        onChange={(e) => handleImmigrationCodeChange(e)}
+        {...register('value')}
         id="immigration-code"
         rows={8}
-        className="block w-full rounded-lg border border-ui-main/50 bg-ui-foundation-light p-2.5 text-sm text-ui-main focus:border-accent focus:ring-1 focus:ring-accent"
-      ></textarea>
-      <Button
-        disabled={!isSaveButtonEnabled}
-        onClick={handleSaveButton}
-        className="mt-2 w-min"
-      >
+        className={cn(
+          'block w-full rounded-lg border border-ui-main/50 bg-ui-foundation-light p-2.5 text-sm text-ui-main focus:border-accent focus:ring-1 focus:ring-accent',
+          !isValid && 'bg-incorrect/30',
+        )}
+      />
+      <Button disabled={!isValid} type="submit" className="mt-2 w-min">
         <SaveIcon className="mr-2 h-6 w-6 cursor-pointer" />
         Save
       </Button>
-    </div>
+    </form>
   )
 }
